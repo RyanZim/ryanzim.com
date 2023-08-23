55aa180055eb54c144d7c2d250ae5955138d52c8
Ryan Zimmerman <opensrc@ryanzim.com>
2023-08-23 19:04:30 -0400

    Rework site

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,5 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-simple-vars'),
-    require('postcss-preset-env')({
-      stage: 0,
-    }),
-    require('postcss-extend-rule'),
   ],
 };
