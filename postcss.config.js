cd772f4587441ec9de349946d102268b60c50621
Ryan Zimmerman <opensrc@ryanzim.com>
2023-08-24 15:57:25 -0400

    Format code

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-simple-vars'),
-  ],
+  plugins: [require('postcss-import'), require('postcss-simple-vars')],
 };
